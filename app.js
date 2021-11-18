5b8858b36d0fb43bdb7d211cb88ee875cf460456
frgs_ph <sfrancisrussell1@gmail.com>
2021-11-18 13:48:23 +0400

    Finished implementing  article.ejs

--- app.js
+++ app.js
@@ -4,26 +4,38 @@ const router = require('./apis/routes.js');
 const app = express();
 app.use(express.static('public'));
 app.use(router);
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
- res.render('index'); //no need for ejs extension
+    res.render('index'); 
+    //no need for ejs extension
 });
 //route for contacts
 app.get('/contacts', (req, res) => {
     res.render('contacts');
    });
+//route for contacts
 app.get('/register', (req, res) => {
     res.render('register');
    });
+//route for contacts
 app.get('/login', (req, res) => {
     res.render('login');
 });
+//route for contacts
 app.get('/api/catalogue', (req, res) => {
     res.render('catalogue');
 });
+//route for contacts
+app.get('/api/article', (req, res) => {
+    res.render('article');
+});
+//route for contacts
 app.post('/api/register', function (req, res) {
     res.send("Register Test");
 } );
+//route for contacts
 app.get('/api/login', function (req, res) {
     res.send("Login Test");
 } );
